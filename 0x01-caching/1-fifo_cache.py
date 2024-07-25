909c43b66670865e708d517092507736bd87c1a2
Queenette Jonas <97518755+Queenette23@users.noreply.github.com>
2024-07-25 08:33:28 +0100

    Add files via upload

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Module: FIFOCache """
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class FIFOCache (BaseCaching):
+    """A FIFO cache implementation"""
+
+    def __init__(self):
+        """Creates a cache instance
+        """
+        super().__init__()
+        self.cache_keys = []
+
+    def put(self, key, item):
+        """Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        size = len(self.cache_data)
+        key_exists = key in self.cache_data
+        if size >= BaseCaching.MAX_ITEMS and not key_exists:
+            discard_key = self.cache_keys.pop(0)
+            self.cache_data.pop(discard_key)
+            print("DISCARD: {}".format(discard_key))
+        if key_exists:
+            self.cache_keys.remove(key)
+        self.cache_data[key] = item
+        self.cache_keys.append(key)
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None:
+            return None
+        return self.cache_data.get(key, None)
